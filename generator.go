601179cf1e178d73494e7754dbdcde131c250c93
Chris Johnson <chris@cjohnson.io>
2013-11-27 01:04:18 -0800

    Better

--- generator.go
+++ generator.go
@@ -6,7 +6,9 @@ import (
 	"github.com/russross/blackfriday"
 	"hash/crc64"
 	"io/ioutil"
+	"os"
 	"path"
+	"strings"
 	"time"
 )
 
@@ -16,6 +18,7 @@ type post struct {
 	DateDisplay time.Time
 	DateCreated time.Time
 	DateUpdated time.Time
+	Public      bool
 	Checksum    int
 	Tags        []string
 }
@@ -25,34 +28,76 @@ type Generator struct {
 	list     []post
 }
 
-// func Sync(g Generator) {
-// 	srcPath := path.Join(g.rootPath, "src/")
-// 	files, err := ioutil.ReadDir(srcPath)
-// 	if err == nil {
-// 		for _, fileInfo := range files {
-
-// 		}
-// 	}
-// }
-
 type test struct {
 	date time.Time
 }
 
-func saveList(posts_src string) {
+func Generate() {
+	initialize()
+	manifest := GetManifest(os.Getenv("BLOG_SRC"))
+	SaveManifest(os.Getenv("BLOG_POSTS"), strings.Join(manifest, "\n"))
+	GenerateFiles(os.Getenv("BLOG_SRC"), os.Getenv("BLOG_POSTS"))
+}
 
-	posts_data, _ := ioutil.ReadDir(posts_src)
+func initialize() {
+	if os.Getenv("BLOG_SRC") == "" {
+		os.Setenv("BLOG_SRC", "~/blog_posts")
+	}
+	if os.Getenv("BLOG_POSTS") == "" {
+		os.Setenv("BLOG_POSTS", "$GOPATH/web/posts")
+	}
+}
 
-	for index, _ := range posts_data {
-		fmt.Println("hi %i", index)
+func SaveManifest(posts_target string, manifestJson string) {
+	os.MkdirAll(posts_target, os.ModePerm)
+	manifestFile, _ := os.Create(path.Join(posts_target, "manifest.json"))
+	manifestFile.WriteString(manifestJson)
+}
+
+func GetManifest(posts_src string) []string {
+	postsData, _ := ioutil.ReadDir(posts_src)
+
+	manifestJson := make([]string, 1)
+
+	for _, postData := range postsData {
+		newPost := post{Shortname: Shortname(postData.Name()), DateCreated: time.Now(), Public: false}
+
+		jsonPost, err := json.Marshal(newPost)
+		if err == nil {
+			manifestJson = append(manifestJson, string(jsonPost))
+		}
 	}
 
-	newPost := post{Title: "The Man Who Saved My Life", Shortname: "cigarettes", DateCreated: time.Now()}
+	return manifestJson
+}
 
-	jsonPost, err := json.Marshal(newPost)
-	if err == nil {
-		fmt.Println("rawjson = " + string(jsonPost))
+func GenerateFiles(posts_src string, posts_target string) {
+	postsData, _ := ioutil.ReadDir(posts_src)
+
+	outDir := path.Join(posts_target, "out")
+
+	os.MkdirAll(outDir, os.ModePerm)
+	fmt.Println("outdir = ", outDir)
+
+	for _, postData := range postsData {
+		fileName := path.Join(posts_src, postData.Name())
+		file, err := ioutil.ReadFile(fileName)
+
+		if err == nil {
+			fileHtml := string(blackfriday.MarkdownBasic(file))
+			outPath := path.Join(outDir, Shortname(postData.Name())+".html")
+			err := ioutil.WriteFile(outPath, []byte(fileHtml), os.ModePerm)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println(err)
+		}
 	}
+}
+
+func Shortname(str string) string {
+	return strings.Split(str, ".")[0]
 }
 
 func Compile(root string, shortname string) string {
