cf409f8a3c1427d0138f8672a4c5eca7a695e32c
Chris Johnson <chris@cjohnson.io>
2013-11-29 19:56:29 -0700

    awefawfe

--- generator.go
+++ generator.go
@@ -13,6 +13,18 @@ import (
 	"time"
 )
 
+var _ = crc64.MakeTable
+
+func init() {
+	if os.Getenv("BLOG_SRC") == "" {
+
+		os.Setenv("BLOG_SRC", "~/blog_posts")
+	}
+	if os.Getenv("BLOG_POSTS") == "" {
+		os.Setenv("BLOG_POSTS", "$GOPATH/web/posts")
+	}
+}
+
 type post struct {
 	Title       string
 	Shortname   string
@@ -38,20 +50,10 @@ type test struct {
 }
 
 func Generate(manifest bool) {
-	initialize()
 	if manifest {
 		SaveManifest()
 	}
-	GenerateFiles()
-}
-
-func initialize() {
-	if os.Getenv("BLOG_SRC") == "" {
-		os.Setenv("BLOG_SRC", "~/blog_posts")
-	}
-	if os.Getenv("BLOG_POSTS") == "" {
-		os.Setenv("BLOG_POSTS", "$GOPATH/web/posts")
-	}
+	GenerateHtml()
 }
 
 func PublicPosts() []post {
@@ -67,14 +69,23 @@ func PublicPosts() []post {
 }
 
 func GenerateHtml() {
+	posts_src := path.Join(os.Getenv("BLOG_SRC"))
+	posts_target := path.Join(os.Getenv("BLOG_POSTS"), "out")
+	os.MkdirAll(posts_target, os.ModePerm)
 	posts := PublicPosts()
 	for _, post := range posts {
-
+		if post.Public {
+			html := Compile(posts_src, post.Shortname)
+			outPath := path.Join(posts_target, post.Shortname+".html")
+			err := ioutil.WriteFile(outPath, []byte(html), os.ModePerm)
+			if err != nil {
+				fmt.Println("Fuck, an err ", err)
+			}
+		}
 	}
 }
 
 func ManifestBytes() []byte {
-	initialize()
 	posts_target := os.Getenv("BLOG_POSTS")
 	fullpath := path.Join(posts_target, "manifest.json")
 	file, err := ioutil.ReadFile(fullpath)
@@ -87,7 +98,6 @@ func ManifestBytes() []byte {
 }
 
 func Manifest() []post {
-	initialize()
 	posts_target := os.Getenv("BLOG_POSTS")
 	fullpath := path.Join(posts_target, "manifest.json")
 	file, err := ioutil.ReadFile(fullpath)
@@ -124,7 +134,6 @@ func WriteManifest(posts_target string, manifest []post) {
 }
 
 func SaveManifest() {
-	initialize()
 	posts_src := os.Getenv("BLOG_SRC")
 	posts_target := os.Getenv("BLOG_POSTS")
 
@@ -140,47 +149,16 @@ func SaveManifest() {
 	WriteManifest(posts_target, manifest)
 }
 
-func GenerateFiles() {
-	initialize()
-	posts_src := os.Getenv("BLOG_SRC")
-	posts_target := os.Getenv("BLOG_POSTS")
-
-	postsData, _ := ioutil.ReadDir(posts_src)
-
-	outDir := path.Join(posts_target, "out")
-
-	os.MkdirAll(outDir, os.ModePerm)
-	fmt.Println("outdir = ", outDir)
-
-	for _, postData := range postsData {
-		fileName := path.Join(posts_src, postData.Name())
-		file, err := ioutil.ReadFile(fileName)
-
-		if err == nil {
-			fileHtml := string(blackfriday.MarkdownBasic(file))
-			outPath := path.Join(outDir, Shortname(postData.Name())+".html")
-			err := ioutil.WriteFile(outPath, []byte(fileHtml), os.ModePerm)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println(err)
-		}
-	}
-}
-
 func Shortname(str string) string {
 	return strings.Split(str, ".")[0]
 }
 
 func Compile(root string, shortname string) string {
 	path := path.Join(root, shortname+".md")
-
 	file, err := ioutil.ReadFile(path)
 
-	checksum := crc64.Checksum(file, crc64.MakeTable(crc64.ISO))
-
-	fmt.Println(checksum)
+	// checksum := crc64.Checksum(file, crc64.MakeTable(crc64.ISO))
+	// fmt.Println(checksum, "awef")
 
 	if err == nil {
 		return string(blackfriday.MarkdownBasic(file))
